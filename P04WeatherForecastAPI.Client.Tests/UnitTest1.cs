d9e53fb48c2076a92ac13c64fb5183c854bb2e07
MilczarekJan <101673349+MilczarekJan@users.noreply.github.com>
2023-11-13 16:56:20 +0100

    Dodanie projektu testow

---
+++ P04WeatherForecastAPI.Client.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace P04WeatherForecastAPI.Client.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
